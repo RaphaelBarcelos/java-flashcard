abff58d5d11f5619063f3809ef9f771820519b8f
Raphael <raphael22522@outlook.com>
2025-05-19 16:41:12 -0300

    Spring Boot Initial Config

---
+++ lumina-flashcards/src/test/java/org/example/luminaflashcards/LuminaFlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.luminaflashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuminaFlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
